c51b67c2d5e7d6711d80d66e1ded92ecad7f9ee5
OmarURP <roblespereyraomarulises@gmail.com>
2026-01-20 16:21:53 -0600

    Se agrega instalacion para Colab

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ setup(
     name="ImtaTURB",
     version="0.1.0",
     description="Librería de operaciones para análisis de turbulencia",
-    author="Tu Nombre",  # Cambia esto
-    author_email="tu.email@example.com",  # Cambia esto
+    author="OmarURP",
+    author_email="roblespereyraomarulises@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "pandas",
-        "matplotlib",
-        "scipy",
-        # Agrega aquí otras dependencias que necesite tu módulo
+        "numpy>=1.20.0",
+        "pandas>=1.3.0",
+        "matplotlib>=3.4.0",
+        "scipy>=1.7.0",
     ],
     python_requires=">=3.6",
 )
